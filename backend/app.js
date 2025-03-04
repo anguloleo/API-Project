951c6c109d6bdac569d8689c9be42f341769c842
Leo Angulo <anguloleo@gmail.com>
2025-03-04 11:41:16 -0800

    completed setup

---
+++ backend/app.js
@@ -0,0 +1,57 @@
+const express = require('express');
+require('express-async-errors');
+const morgan = require('morgan');
+const cors = require('cors');
+const csurf = require('csurf');
+const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
+const routes = require('./routes');
+
+//check if in production mode
+const { environment } = require('./config');
+const isProduction = environment === 'production';
+
+//initialize Express application
+const app = express();
+
+//logging requests and responses
+app.use(morgan('dev'));
+
+//parse cookies and json bodies
+app.use(cookieParser());
+app.use(express.json());
+
+// Security Middleware
+if (!isProduction) {
+    // enable cors only in development
+    app.use(cors());
+  }
+  
+  // helmet helps set a variety of headers to better secure your app
+  app.use(
+    helmet.crossOriginResourcePolicy({
+      policy: "cross-origin"
+    })
+  );
+  
+  // Set the _csrf token and create req.csrfToken method
+  app.use(
+    csurf({
+      cookie: {
+        secure: isProduction,
+        sameSite: isProduction && "Lax",
+        httpOnly: true
+      }
+    })
+  );
+
+  //connect to routes
+  app.use(routes);
+
+
+
+
+
+
+
+  module.exports = app;
